b7d7c787de3222bdd716cb9d982f63750e665724
Nishant Khunt <knishantkumar008@gmail.com>
2025-09-20 09:40:04 +0000

    same hi hai kuch change nahi hua

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: "YOUR_API_KEY_HERE",
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
