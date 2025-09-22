d77288bc56776bf9640a9d10aa5bac72b7b08b29
Nishant Khunt <knishantkumar008@gmail.com>
2025-09-22 10:26:51 +0000

    font-explorar-o5gyc6b36-knishantkumar008-sys-projects.vercel.app ye vala

--- src/lib/placeholder-images.ts
+++
@@ -1,4 +0,0 @@
-import { placeholderImages as allPlaceholderImages } from '@/lib/image-data';
-import type { ImagePlaceholder } from '@/lib/image-data';
-
-export const PlaceHolderImages: ImagePlaceholder[] = allPlaceholderImages;
