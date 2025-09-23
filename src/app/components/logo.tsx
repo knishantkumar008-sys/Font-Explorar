af8705405b54e7cd3f1b042172d34fded0e11b12
Nishant Khunt <knishantkumar008@gmail.com>
2025-09-23 08:11:03 +0000

    image me jo logo hai voh abhi bhi sahi se view nahi ho raha hai

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -7,7 +7,6 @@ export function Logo() {
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="text-white"
     >
       <path
         d="M14 12V10H8V22H10V20H14V18H10V16H14V14H10V12H14ZM24 12V10H16V22H24V20H18V17H22V15H18V12H24Z"
